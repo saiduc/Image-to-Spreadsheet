49e8a894276acc503f1a34637441afa7459dcbbb
SaiDuc <saipandian97@gmail.com>
2020-02-19 22:40:13 +0000

    Converted True/False array to 1/0 array so cond formatting is easier

--- create.py
+++ create.py
@@ -8,6 +8,17 @@ im = im.convert('1')
 image_data = np.array(im)
 x, y = image_data.shape
 
-df = pd.DataFrame(data=image_data, index=None)
+
+new_image_data = np.zeros((x,y))
+for i in range(x):
+    for j in range(y):
+        if image_data[i][j] == True:
+            new_image_data[i][j] = 1
+        elif image_data[i][j] == False:
+            new_image_data[i][j] = 0
+    
+
+df = pd.DataFrame(data=new_image_data, index=None)
 with pd.ExcelWriter("image.xlsx") as writer:
     df.to_excel(writer)
+    worksheet = writer.sheets['Sheet1']
