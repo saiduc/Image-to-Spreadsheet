4f434295d19378de67caf8df5ac82a6b09d11b09
SaiDuc <saipandian97@gmail.com>
2020-02-19 22:26:13 +0000

    Added writing to excel file with pandas

--- create.py
+++ create.py
@@ -7,3 +7,7 @@ im = Image.open("psyduck_profile.jpg")
 im = im.convert('1')
 image_data = np.array(im)
 x, y = image_data.shape
+
+df = pd.DataFrame(data=image_data, index=None)
+writer = pd.ExcelWriter('image.xlsx', engine='xlsxwriter')
+df.to_excel(writer, sheet_name='Sheet1')
