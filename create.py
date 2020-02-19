31b9a3e29421816c47b83833ecff2486a34186bc
SaiDuc <saipandian97@gmail.com>
2020-02-19 23:31:04 +0000

    Added automatic conditional formatting for output spreadsheet

--- create.py
+++ create.py
@@ -3,22 +3,26 @@ import numpy as np
 import pandas as pd
 from PIL import Image
 
-im = Image.open("psyduck_profile.jpg")
+image = "psyduck_profile.jpg"
+im = Image.open(image)
 im = im.convert('1')
 image_data = np.array(im)
 x, y = image_data.shape
 
 
-new_image_data = np.zeros((x,y))
+new_image_data = np.zeros((x, y))
 for i in range(x):
     for j in range(y):
         if image_data[i][j] == True:
             new_image_data[i][j] = 1
         elif image_data[i][j] == False:
             new_image_data[i][j] = 0
-    
 
-df = pd.DataFrame(data=new_image_data, index=None)
-with pd.ExcelWriter("image.xlsx") as writer:
-    df.to_excel(writer)
-    worksheet = writer.sheets['Sheet1']
+df = pd.DataFrame(data=new_image_data, index=None, columns=None)
+writer = pd.ExcelWriter('image.xlsx', engine="xlsxwriter")
+df.to_excel(writer, sheet_name='Sheet1', index=None, header=None)
+
+workbook = writer.book
+worksheet = writer.sheets['Sheet1']
+worksheet.conditional_format('A1:XFD1048576', {'type': '2_color_scale'})
+writer.save()
